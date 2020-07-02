0f1f61c8c6ac32d8d7812b51cd84337b1710c169
Rashmi Ranjan Rout <45759653+rrout@users.noreply.github.com>
2020-07-02 22:38:15 +0530

    Update rpubsub.h

--- include/rpubsub.h
+++ include/rpubsub.h
@@ -7,14 +7,42 @@
 #define MAX_TOPIC               70
 #define MAX_SUBSCRIBER_THREAD   MAX_TOPIC 
 
+/* TOPIC */
+
+/**/
+typedef enum {
+         PUBSUB_DATA_TYPE_PUB_REQ_TOPIC_ENQUEUE,
+         PUBSUB_DATA_TYPE_PUB_RESP_TOPIC_ENQUEUE_PASS,
+         PUBSUB_DATA_TYPE_PUB_RESP_TOPIC_ENQUEUE_FAIL,
+         PUBSUB_DATA_TYPE_PUB_REQ_TOPIC_ENQUEUE_ROLLBACK,
+         PUBSUB_DATA_TYPE_PUB_RESP_TOPIC_ENQUEUE_ROLLBACK_PASS,
+         PUBSUB_DATA_TYPE_PUB_RESP_TOPIC_ENQUEUE_ROLLBACK_FAIL,
+         PUBSUB_DATA_TYPE_SUB_REQ_REGISTER,
+         PUBSUB_DATA_TYPE_SUB_RESP_REGISTER_PASS,
+         PUBSUB_DATA_TYPE_SUB_RESP_REGISTER_FAIL,
+         PUBSUB_DATA_TYPE_SUB_REQ_DEREGISTER,
+         PUBSUB_DATA_TYPE_SUB_RESP_DEREGISTER_PASS,
+         PUBSUB_DATA_TYPE_SUB_RESP_DEREGISTER_FAIL,
+         PUBSUB_DATA_TYPE_SUB_REQ_RESP_MAX
+}pubSubDataType_t;
+
+/**/
+typedef struct pubSubData_s{
+         pubSubDataType_t dataType;
+         rPubSubTopic_t *topic;
+}pubSubData_t;
+
+/**/
 typedef struct rPubSubBusContext_s {
         struct sockaddr_in busAddr;
 }rPubSubBusContext_t;
 
+/**/
 typedef struct rPubSubBus_s {
         int socket;
 }rPubSubBus_t;
 
+/**/
 typedef struct _rPubSubSubscriber_s {
     int SubscriberId;
     char SubscriberName[MAX_NAME_LEN];
@@ -26,6 +54,7 @@ typedef struct _rPubSubSubscriber_s {
     struct _rPubSubSubscriber_s *next;
 }rPubSubSubscriber_t;
 
+/**/
 rPubSubBus_t    *rPubSubBusConnect(rPubSubBusContext_t *rPubSubBus);
 rPubSubStatus_t *rPubSubPublish(rPubSubBus_t *rPubSubBus, rPubSubTopic_t *topic);
 
